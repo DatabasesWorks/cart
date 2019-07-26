a7ceaffb831feef6d504be0ab4202ce6957fde89
qm <q@q.com>
2019-07-26 12:57:00 +0800

    number

--- order.cpp
+++ order.cpp
@@ -8,14 +8,38 @@ order::order(QWidget *parent) : QWidget(parent)
     for (int i=0; i< 4; ++i)
         for (int j =0; j<3; ++j)
         {
-            QLabel* label = new QLabel(QString::number(i));
+            QLabel* label = new QLabel();
+
+            auto labellay = new QGridLayout(label);
+
             QImage img(":/img/cart2.jpg");
-            label->setPixmap(QPixmap::fromImage(img));
+            img = img.scaled(200,200);
+            QLabel* pic = new QLabel();
+            pic->setPixmap(QPixmap::fromImage(img));
+
+            labellay->addWidget(pic, 0, 0, 1, 3);
+
+            auto left = new QPushButton("left");
+            labellay->addWidget(left, 1, 0, 1, 1);
+            auto right = new QPushButton("right");
+            labellay->addWidget(right, 1, 2, 1, 1);
+
+            QLabel* number = new QLabel("0");
+            number->setAlignment(Qt::AlignCenter);
+            connect(left, &QPushButton::clicked, [=]()
+            {
+                int num = number->text().toInt();
+                number->setText(QString::number(--num));
+            });
+            connect(right, &QPushButton::clicked, [=]()
+            {
+                int num = number->text().toInt();
+                number->setText(QString::number(++num));
+            });
+            labellay->addWidget(number, 1, 1, 1, 1);
 
             label->resize(200,200);
             layout->addWidget(label, j, i);
-            auto labellay = new QGridLayout(label);
-            labellay->addWidget(new QPushButton("inside"));
         }
 
 
