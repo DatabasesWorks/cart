6ba0566c944c3a1b24309311088bf17b23653798
qm <q@q.com>
2019-07-26 17:38:59 +0800

    stacked widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,19 +5,46 @@
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
-    auto *layout = new QGridLayout(this);
-    for (int i=0; i< 4; ++i)
-        for (int j =0; j<3; ++j)
-        {
-            auto btn = new QPushButton();
-            layout->addWidget(btn, j, i);
-            m_btnOrders.push_back(btn);
-            int index = m_btnOrders.size()-1;
-            connect(btn, &QPushButton::clicked, [=]()
+    auto layoutall = new QGridLayout(this);
+    auto left = new QPushButton;
+    auto right = new QPushButton;
+    auto central = new QStackedWidget;
+    layoutall->addWidget(left,0,0);
+    layoutall->addWidget(right,0,2);
+    layoutall->addWidget(central,0,1);
+
+    connect(left, &QPushButton::clicked, [=]()
+    {
+        int id = central->currentIndex();
+        if (id >0)
+            central->setCurrentIndex(id-1);
+    });
+
+    connect(right, &QPushButton::clicked, [=]()
+    {
+        int id = central->currentIndex();
+        if (id < central->count()-1)
+            central->setCurrentIndex(id+1);
+    });
+
+    for (int stackid = 0; stackid < 3; ++stackid)
+    {
+        auto widget = new QWidget;
+        central->addWidget(widget);
+        auto *layout = new QGridLayout(widget);
+        for (int i=0; i< 3; ++i)
+            for (int j =0; j<3; ++j)
             {
-                emit updateOrder(index);
-            });
-        }
+                auto btn = new QPushButton(QString::number(stackid*9 + i*3 +j));
+                layout->addWidget(btn, j, i);
+                m_btnOrders.push_back(btn);
+                int index = m_btnOrders.size()-1;
+                connect(btn, &QPushButton::clicked, [=]()
+                {
+                    emit updateOrder(index);
+                });
+            }
+    }
 
 }
 
